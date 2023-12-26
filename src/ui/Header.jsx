565721c1fc8dd8be29167786f787c8df18d1f134
Ahmed Kamel <ahmedkamel.egy5577@gmail.com>
2023-12-26 20:51:12 +0200

    changing directory

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchingOrder from "../features/order/SearchingOrder";
-import UserName from "../features/user/userName";
-
+import UserName from "../features/user/UserName";
 const Header = () => {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
